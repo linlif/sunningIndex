af8967943a9580b43e3939d7435612f731350cf7
linlif <1264954361@qq.com>
2017-08-02 17:30:10 +0800

    Revert "delete useless files"
    This reverts commit b8857e6022800f898e2d1cebd2b12efebe9b566d.

---
+++ scripts/imgSlide.js
@@ -0,0 +1,20 @@
+
+$(function(){
+  $("#jnBrandTab li a").click(function() {
+    $(this).parent().addClass('chos')
+            .siblings().removeClass('chos');
+    var index = $("#jnBrandTab li a").index(this);
+    showBrandList(index);
+    return false
+  }).eq(0).click();
+});
+
+
+//显示不同模块
+function showBrandList(index){
+  // console.log("haha");
+  var $rollObj = $("#jnBrandList");
+  var rollWidth = $rollObj.find("li").outerWidth();
+  rollWidth = rollWidth * 4;//一个版面的宽度
+  $rollObj.stop(true, false).animate({left : -rollWidth*index}, 1000);
+}
